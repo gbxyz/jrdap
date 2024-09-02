4a900e39ad06b291c9c9ec3a7abf9f12412be37d
Gavin Brown <gavin.brown@icann.org>
2024-09-02 11:25:13 +0100

    add install script

--- README.md
+++ README.md
@@ -5,8 +5,18 @@ dependencies** beyond node.js.
 
 # Installation
 
-To install, either copy [`jrdap`](jrdap) somewhere into your path or run `make
+The easiest way to install `jrdap` is run this command:
+
+```
+curl https://raw.githubusercontent.com/gbxyz/jrdap/main/install.js | sudo node
+```
+
+Alternatively, just copy [`jrdap`](jrdap) somewhere into your path, or run `make
 install` to install it into `/usr/local/bin`.
+
+# Usage instructions
+
+Once `jrdap` is installed, run `jrdap --help` to see usage instructions.
 
 # License
 
---
+++ install.js
@@ -0,0 +1,27 @@
+// jrdap install script
+// usage: curl https://raw.githubusercontent.com/gbxyz/jrdap/main/install.js | sudo node
+
+const fs = require('node:fs');
+
+const file = '/usr/local/bin/jrdap';
+
+fetch("https://raw.githubusercontent.com/gbxyz/jrdap/main/jrdap").then(
+    r => r.text().then(install, fail),
+    fail,
+);
+
+function fail(error) {
+    process.stderr.write("Error: " + error + "\n");
+    process.exit(1);
+}
+
+function install(data) {
+    try {
+        fs.writeFileSync(file, data, {mode:0o755});
+        process.stderr.write("jrdap successfully installed to " + file + "!\n");
+
+    } catch (e) {
+        fail(e);
+
+    }
+}
